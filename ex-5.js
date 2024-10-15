c02ffde14301f53606e179ba0bc6692e2c719923
Oat Chana <chanathip0944729842@gmail.com>
2024-10-15 14:27:36 +0700

    feat:add answer es6

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,5 @@
-function sum() {
-  // Start coding here !
+function sum(...rest) {
+  return rest.reduce(((acc,cur) => acc+cur),0)
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
@@ -7,3 +7,4 @@ let result2 = sum(1, 2, 3, 4);
 
 console.log(`Result ${result1}`);
 console.log(`Result ${result2}`);
+
--- ex-6.js
+++ ex-6.js
@@ -2,3 +2,9 @@ let height = undefined;
 let result;
 
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
+
+function isUndefined(data) {
+    return data ?? "Height is not defined"
+}
+
+console.log(isUndefined(result))
